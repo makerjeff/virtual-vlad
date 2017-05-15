deb5d8d968c97cbae95b6963bdf04321d4d02008
Jefferson Wu <makerjeffwu@gmail.com>
2017-05-15 15:00:47 -0700

    current state

--- nlp-play-ml-classify-text.js
+++ nlp-play-ml-classify-text.js
@@ -12,10 +12,33 @@ var training_data = [
     {text: 'Earn more from home', label: 'spam'},
     {text: 'Information now available!!!', label: 'spam'},
     {text: 'Earn easy cash', label: 'spam'},
-    {text: 'Your business trip is confirmed for Monday the 4th', label: 'spam'},
-    {text: ''}
+    {text: 'Your business trip is confirmed for Monday the 4th', label: 'notspam'},
+    {text: 'Project planning - next steps', label: 'notspam'},
+    {text: 'Birthday party next weekend', label: 'notspam'},
+    {text: 'Drinks on Monday?', label: 'notspam'}
 ];
 
-
 // need to have test data:
+var test_data = [
+    {text: 'Drugs for cheap', label: 'spam'},
+    {text: 'Next deadline due Monday', label: 'notspam'},
+    {text: 'Meet me at home?', label: 'notspam'},
+    {text: 'Hang out with someone near you', label: 'spam'}
+];
+
+// add training data
+training_data.forEach(function(elem,ind,arr) {
+    classifier.addDocument(elem.text, elem.label);
+});
+
+// train classifier
+classifier.train();
+
+test_data.forEach(function(elem, ind, arr) {
+    var label_guess = classifier.classify(elem.text);
+    console.log('\n');
+    console.log(elem.text);
+    console.log("label: " + label_guess);
+    console.log(classifier.getClassifications(elem.text));
+});
 
