0a36e5d8291cd672e30e88c7e3f059d835bc5ea6
Jefferson Wu <makerjeffwu@gmail.com>
2017-05-17 11:56:07 -0700

    working on classifying json

---
+++ nlp-play-ml-classify-json.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jefferson.wu on 5/11/17.
+ */
+
+const natural = require('natural');
+const fs = require('fs');
+
+var classifier = new natural.BayesClassifier();
+
+// science and space, or politics.
+
